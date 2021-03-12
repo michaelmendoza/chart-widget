c21605eda5e350feb6f1871e332bf3c64be53d3c
Michael Mendoza <askmichaelmendoza@gmail.com>
2021-03-12 15:01:58 -0500

    Refactor to DataPipeline. Broke up transformEntity into transform, aggreate, and reduce functions.

--- src/Chart/services/ChartDataPipeline.ts
+++ src/Chart/services/ChartDataPipeline.ts
@@ -19,11 +19,11 @@ export enum DataIOTypes {
 export class DataSource {
     cache: any = null;
     
-    fetch(feedName:string, attributes:string[] = [], type : ChartTypes, dataMetric : DataMetrics, historyLength: number, filter: IChartFilter) {
+    fetch(feedName:string, attributes:string[] = [], type : ChartTypes, dataMetric : DataMetrics, historyLength: number, filters: IChartFilter) {
         console.log('Fetching Data from Data Source - feedName: ' + feedName + ', attribute: ' + attributes[0]);
 
         return ChartDataService.fetchEntityDataByFeed(feedName).then((res) => {
-            let args = { inputData: res, attributes, type, inputType: DataIOTypes.Entity, outputType: DataIOTypes.Entity, dataMetric, historyLength, filter }
+            let args = { inputData: res, attributes, type, inputType: DataIOTypes.Entity, outputType: DataIOTypes.Entity, dataMetric, historyLength, filters }
 
             switch(type) {
                 case ChartTypes.Number:
@@ -65,20 +65,17 @@ export class DataPipeline {
     inputData: any;
     attributes: string[];
     type: ChartTypes;
-    filter: any;
+    filters: any;
     inputType : DataIOTypes;
     outputType : DataIOTypes;
     dataMetric : DataMetrics;
     historyLength: number;
-    a: any;
-    b: any;
 
-    //constructor(inputData: any, attributes: string[], type: ChartTypes, inputType: DataIOTypes, outputType: DataIOTypes, dataMetric: DataMetrics, historyLength: number = 30, filter: IChartFilter) {
-    constructor({ inputData, attributes, type, inputType, outputType, dataMetric, historyLength, filter } : any) {
+    constructor({ inputData, attributes, type, inputType, outputType, dataMetric, historyLength, filters } : any) {
         this.inputData = inputData;
         this.attributes = attributes;
         this.type = type;
-        this.filter = filter;
+        this.filters = filters;
         this.inputType = inputType;
         this.outputType = outputType;
         this.dataMetric = dataMetric;
@@ -90,89 +87,84 @@ export class DataPipeline {
 
         switch(this.inputType) {
             case DataIOTypes.Entity:
-                this.filterEntity();
-                return this.transformEntity();
+                let data; 
+                data = this.filter(this.inputData);
+                data = this.transform(data);
+                data = this.aggregate(data);
+                data = this.reduce(data);
+                return data;
         }
     }
 
-    filterEntity() {
-        let filter;
-        switch(this.filter.filterType) {
+    filter(inputData : any) {
+        let filters;
+        switch(this.filters.filterType) {
             case FilterTypes.Circle:
-                filter = { circle: this.filter.circle }
+                filters = { circle: this.filters.circle }
                 break;
             case FilterTypes.Shapes:
-                filter = { geoJson: this.filter.shapes }
+                filters = { geoJson: this.filters.shapes }
                 break;
         }
 
-        this.inputData = geoFilter(this.inputData, filter);
+        return geoFilter(inputData, filters);
     }
 
-    transformEntity() {
-        let data = [];
+    transform = (data : any) => {
         switch(this.outputType) {
-            case DataIOTypes.Entity:
-                return this.inputData;
-
             case DataIOTypes.AttributeArray:
-                return this.inputData.map((item : any)=>{
+                return data.map((item : any)=>{
                     return { id:item.id, name:item.name, x:item.geo[0], y:item.geo[1], ...item.attr, time:item.time }
                 })
-
+            
             case DataIOTypes.Number:
-                // Transform EntityData array to an array of attibute values 
-                data = EntityDataToDataArray(this.inputData, this.attributes[0]);
-                // Calculate relevant statistic
-                return ReduceDataArrayToMetric(data, this.dataMetric);
-                
             case DataIOTypes.Stats:
-                // Transform EntityData array to an array of attibute values 
-                data = EntityDataToDataArray(this.inputData, this.attributes[0]);
-                // Calculate relevant statistic
-                return ReduceDataArrayToStats(data, this.dataMetric);
-
             case DataIOTypes.XYPointArray:
                 // Transform EntityData array to an array of attibute values 
-                data = EntityDataToDataArray(this.inputData, this.attributes[0]);
+                return EntityDataToDataArray(data, this.attributes[0]);
+
+            case DataIOTypes.XMultiYPointArray: 
+                // Transform EntityData array to an matrix (array of array) of attibute values 
+                return EntityDataToDataMatrix(data, this.attributes);
+
+            case DataIOTypes.XtYPointArray:
+                return EntityDataToTimeSeriesData(data, this.attributes[0]);
+            
+            default:
+                return data;
+        } 
+    }
+
+    aggregate = (data : any) => {
+        switch(this.outputType) {
+            case DataIOTypes.XYPointArray:
                 // Transform and group array data into to binned data
                 return GroupDataArrayByValue(data);
 
             case DataIOTypes.XMultiYPointArray: 
-                // Transform EntityData array to an matrix (array of array) of attibute values 
-                data = EntityDataToDataMatrix(this.inputData, this.attributes);
                 // Transform and group matrix data into to binned data
                 return GroupDataMatrixByValue(data);    
-
-            case DataIOTypes.XtYPointArray:
-                return EntityDataToTimeSeriesData(this.inputData, this.attributes[0]);
                 
             case DataIOTypes.XtMultiPointArray:
-               return GroupEntityDataByDate(this.inputData, this.attributes, this.dataMetric, this.historyLength);
+               return GroupEntityDataByDate(data, this.attributes, this.dataMetric, this.historyLength);
+
+            default:
+                return data;
         }
     }
-}
 
-/**
- * Group by Value ie. Histogram
- * Group by Date and then sum, avg, std dev
- * 
- * Plots:
- * 1) X: Bins, Y: Value Count 
- * 2) X: Time, Y: Value - Metric (Sum, Avg, StdDev, Median)
- * 3) ScatterPlot: (Comparision) X:Attr1 Values, Y:Attr2 Values
- * 4) ScatterPlot: X: Time, Y: Value 
- * 
- */
-
-const ChartDataAdaptor = () => {
-
-}
-
-const ChartDataFilters = () => {
-
-}
-
-const ChartDataTransform = () => {
-    
+    reduce = (data : any) => {
+        switch(this.outputType) {
+            case DataIOTypes.Number:
+                // Calculate relevant statistic
+                return ReduceDataArrayToMetric(data, this.dataMetric);
+                
+            case DataIOTypes.Stats:
+                // Calculate relevant statistic
+                return ReduceDataArrayToStats(data, this.dataMetric);
+            
+            default: 
+                return data;
+        }
+    }
 }
