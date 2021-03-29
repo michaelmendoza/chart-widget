7e59832eb91fdc1ac78c7e51658792cba76b903f
Michael Mendoza <askmichaelmendoza@gmail.com>
2021-03-29 10:22:49 -0400

    Refactored Chart to ChartWidget

---
+++ src/ChartWidget/ChartWidgetApp.tsx
@@ -0,0 +1,14 @@
+import ChartWidget from './components/ChartWidget';
+import React from 'react';
+
+/**
+ * Wrapper for ChartWidget component 
+ */
+const ChartWidgetApp = () => {
+    
+    return ( 
+        <ChartWidget></ChartWidget>
+    )
+}
+
+export default ChartWidgetApp;
